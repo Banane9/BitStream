54731452c4fbcf44d55557c933299e15e23f282b
Arne Kiesewetter <banane1996@gmx.net>
2015-04-16 19:11:26 +0200

    Implement read

---
+++ PartialStream/BitNum.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitStream
+{
+    public struct BitNum
+    {
+        public static readonly BitNum MaxValue = (BitNum)8;
+        public static readonly BitNum MinValue = (BitNum)1;
+
+        private readonly byte value;
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="BitNum"/> struct with the given value.
+        /// <para/>
+        /// Value will be truncated to the MaxValue if it's larger or rised to the MinValue.
+        /// </summary>
+        public BitNum(byte value)
+        {
+            this.value = Math.Min(MaxValue, Math.Max(MinValue, value));
+        }
+
+        public byte GetBitPos()
+        {
+            return (byte)(1 << (value - 1));
+        }
+
+        #region Casts
+
+        #region From this
+
+        public static implicit operator sbyte(BitNum @this)
+        {
+            return (sbyte)@this.value;
+        }
+
+        public static implicit operator byte(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator short(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator ushort(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator int(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator uint(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator long(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator ulong(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator decimal(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator float(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        public static implicit operator double(BitNum @this)
+        {
+            return @this.value;
+        }
+
+        #endregion From this
+
+        #region To this
+
+        public static explicit operator BitNum(sbyte that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(byte that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(short that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(ushort that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(int that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(uint that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(long that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(ulong that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(decimal that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(float that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        public static explicit operator BitNum(double that)
+        {
+            return new BitNum((byte)that);
+        }
+
+        #endregion To this
+
+        #endregion Casts
+    }
+}
---
+++ PartialStream/BitStream.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace BitStream
+{
+    /// <summary>
+    /// Represents a
+    /// </summary>
+    public sealed class BitStream : Stream
+    {
+        private readonly Stream stream;
+
+        public Stream UnderlayingStream
+        {
+            get { return stream; }
+        }
+
+        public override bool CanRead
+        {
+            get { return stream.CanRead; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return stream.CanSeek; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return stream.CanWrite; }
+        }
+
+        public override void Flush()
+        {
+            stream.Flush();
+        }
+
+        public override long Length
+        {
+            get { return stream.Length; }
+        }
+
+        public override long Position
+        {
+            get { return stream.Position; }
+            set { stream.Position = value; }
+        }
+
+        public BitNum BitPosition { get; set; }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            if (BitPosition == BitNum.MaxValue)
+                return stream.Read(buffer, offset, count);
+
+            return (int)(BitRead(buffer, offset, (uint)count * BitNum.MaxValue) / BitNum.MaxValue);
+        }
+
+        public ulong BitRead(byte[] buffer, int offset, ulong count)
+        {
+            ulong bitsRead = 0;
+            while (count / BitNum.MaxValue > 0)
+            {
+                var nextByte = ReadByte();
+
+                if (nextByte < 0)
+                    buffer[offset] = 0;
+                else
+                {
+                    buffer[offset] = (byte)nextByte;
+                    bitsRead += BitNum.MaxValue;
+                }
+
+                ++offset;
+            }
+
+            byte lastByte;
+            var bits = (BitNum)(count % BitNum.MaxValue);
+            if (BitRead(out lastByte, bits))
+                bitsRead += bits;
+
+            buffer[offset] = lastByte;
+
+            return bitsRead;
+        }
+
+        public override int ReadByte()
+        {
+            byte buffer;
+            return BitRead(out buffer, BitNum.MaxValue) ? buffer : -1;
+        }
+
+        private byte currentByte;
+
+        public bool BitRead(out byte buffer, BitNum bits)
+        {
+            if (BitPosition == BitNum.MaxValue && bits == BitNum.MaxValue)
+            {
+                var readByte = stream.ReadByte();
+                buffer = (byte)(readByte < 0 ? 0 : readByte);
+                return !(readByte < 0);
+            }
+
+            buffer = 0;
+            for (byte i = 0; i < bits; ++i)
+            {
+                if (BitPosition == BitNum.MaxValue)
+                {
+                    var readByte = stream.ReadByte();
+
+                    if (readByte < 0)
+                        return i > 0;
+
+                    currentByte = (byte)readByte;
+                    BitPosition = BitNum.MinValue;
+                }
+
+                buffer |= (byte)(currentByte & BitPosition.GetBitPos());
+                BitPosition = new BitNum((byte)(BitPosition + 1));
+            }
+
+            return true;
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return stream.Seek(offset, origin);
+        }
+
+        public override void SetLength(long value)
+        {
+            stream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            if (BitPosition == BitNum.MaxValue)
+                stream.Write(buffer, offset, count);
+
+            BitWrite(buffer, offset, (uint)count * 8);
+        }
+
+        private void BitWrite(byte[] buffer, int offset, ulong p)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="BitStream"/> class with the given underlaying stream.
+        /// </summary>
+        /// <param name="underlayingStream">The underlaying stream to work on.</param>
+        public BitStream(Stream underlayingStream)
+        {
+            BitPosition = BitNum.MaxValue;
+            stream = underlayingStream;
+        }
+    }
+}
